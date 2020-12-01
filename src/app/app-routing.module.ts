494129b21ece73ef3f1bc08bc6920ed9a48179cd
Paul Chesa <paulchesa1@gmail.com>
2020-12-01 17:08:40 +0300

    started log in, sign up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LogInComponent } from './log-in/log-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"log-in", component: LogInComponent},
+  {path: "sign-up", component: SignUpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
